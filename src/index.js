b7cfb196c6f262c87f4232c4bd03ab0bfc48708f
Domagoj Kriskovic <domagoj.kriskovic@gmail.com>
2017-05-10 14:12:15 +0200

    delay debounce

--- src/index.js
+++ src/index.js
@@ -1,105 +1,28 @@
 import request from 'request'
-/*
-[
-	{
-		case: CHAT_INPUT_SUBMITTED,
-		dispatch: ADD_MESSAGE_REQUESTED
-	},
-	{
-		case: CHAT_INPUT_SUBMITTED,
-		debounce: 400,
-		delay: 400,
-		dispatch: (a) => ({ ADD_MESSAGE_REQUESTED }),
-	},
-	{
-		case: [
-			CHAT_INPUT_SUBMITTED,
-			MESSANGER_INPUT_SUBMITED
-		],
-		dispatch: ADD_MESSAGE_REQUESTED
-	},
-	{
-		case: ADD_MESSAGE_REQUESTED,
-		post: {
-			url: something,
-			onSuccess: ADD_MESSAGE_SUCCEEDED,
-			onFail: ADD_MESSAGE_FAILED
-		}
-	},
-	{
-		case: SEARCH_INPUT_CHARACTER_ENTERED,
-		debounce: 500,
-		get: (a, s) => ({
-			url: autocomplete/a.payload,
-			cancelWhen: [
-				SEARCH_INPUT_CHARACTER_ENTERED,
-				SEARCH_INPUT_BLURED
-			],
-			onSuccess: AUTOCOMPLETE_SUGGESTION
-		})
-	},
-	{
-		case: [
-			CHAT_INPUT_SUBMITTED,
-			MESSANGER_INPUT_SUBMITED
-		],
-		debounce: 1000,
-		post: {
-			url: 'analyticsDb/action.type'
-		}
-	}
-	{
-		case: [
-			CHAT_INPUT_SUBMITTED,
-			MESSANGER_INPUT_SUBMITED
-		],
-		dispatch: { type: ADD_MESSAGE_REQUESTED, message: a.payload }
-	},
-	{
-		case: MESSAGE_NOTIFICATION_RECIEVED,
-		dispatch: { type: ADD_MESSAGE_SUCCEEDED, message: a.payload }
-	},
-	{
-		case: ADD_MESSAGE_REQUESTED,
-		request: {
-			url: url,
-			onSuccess: res => ({ type: ADD_MESSAGE_SUCCEEDED }),
-			onFail: res => ({ type: ADD_MESSAGE_FAILED })
-		}
-	},
-	{
-		case: SEARCH_INPUT_CHARACTER_ENTERED,
-		debounce: 500,
-		post: {
-			url: autocomplete/a.payload,
-			cancelWhen: [
-				SEARCH_INPUT_CHARACTER_ENTERED,
-				SEARCH_INPUT_BLURED
-			],
-			onSuccess: res => ({ type: AUTOCOMPLETE_SUGGESTION })
-		}
-	},
-	{
-		case: SEARCH_INPUT_CHARACTER_ENTERED,
-		dispatch: { type: AUTOCOMPLETE_SUGGESTION, payload: [] }
-	},
-	{
-		case: [
-			CHAT_INPUT_SUBMITTED,
-			MESSANGER_INPUT_SUBMITED
-		],
-		debounce: 1000,
-		request: {
-			url: 'analyticsDb/a.type',
-			onSuccess: null
-			onFail:  null
-		}
-	}
-]
-*/
+
 function forceArray (arr) {
   if (!Array.isArray(arr)) return [arr]
   return arr
+}
+
+
+function dispatch (next, action, { debounce, delay, _debounceTimeoutRefs }) {
+  let dispatch = next
+  if (delay) {
+    dispatch = (action) => setTimeout(() => next(action), delay)
+  }
+
+  if (debounce) {
+    clearTimeout(_debounceTimeoutRefs[action.type])
+    _debounceTimeoutRefs[action.type] = setTimeout(() => {
+      delete _debounceTimeoutRefs[action.type]
+      dispatch(action)
+    }, debounce)
+
+  } else {
+    dispatch(action)
+  }
+  
 }
 
 const orchestration = (config, options) => store => next => action => {
@@ -112,6 +35,10 @@ const orchestration = (config, options) => store => next => action => {
   }
 
   config.forEach(rule => {
+    if (typeof rule._debounceTimeoutRefs !== 'object') {
+      rule._debounceTimeoutRefs = {}
+    }
+
     const ruleConfig = {}
     Object.keys(rule).forEach(ruleKey => {
       if (typeof rule[ruleKey] === 'function') {
@@ -127,7 +54,7 @@ const orchestration = (config, options) => store => next => action => {
         dispatchAction = {...action, type: ruleConfig.dispatch}
       }
       if (action.type === c) {
-        next(dispatchAction)
+        dispatch(next, dispatchAction, ruleConfig)
       }
     })
   })
--- src/index.test.js
+++ src/index.test.js
@@ -1,16 +1,104 @@
 import orchestration from './index'
 import { createStore, applyMiddleware } from 'redux'
 
-function getLastAction(config, options, testAction) {
-  let lastAction
+/*
+[
+	{
+		case: ADD_MESSAGE_REQUESTED,
+		post: {
+			url: something,
+			onSuccess: ADD_MESSAGE_SUCCEEDED,
+			onFail: ADD_MESSAGE_FAILED
+		}
+	},
+	{
+		case: SEARCH_INPUT_CHARACTER_ENTERED,
+		debounce: 500,
+		get: (a, s) => ({
+			url: autocomplete/a.payload,
+			cancelWhen: [
+				SEARCH_INPUT_CHARACTER_ENTERED,
+				SEARCH_INPUT_BLURED
+			],
+			onSuccess: AUTOCOMPLETE_SUGGESTION
+		})
+	},
+	{
+		case: [
+			CHAT_INPUT_SUBMITTED,
+			MESSANGER_INPUT_SUBMITED
+		],
+		debounce: 1000,
+		post: {
+			url: 'analyticsDb/action.type'
+		}
+	}
+	{
+		case: [
+			CHAT_INPUT_SUBMITTED,
+			MESSANGER_INPUT_SUBMITED
+		],
+		dispatch: { type: ADD_MESSAGE_REQUESTED, message: a.payload }
+	},
+	{
+		case: MESSAGE_NOTIFICATION_RECIEVED,
+		dispatch: { type: ADD_MESSAGE_SUCCEEDED, message: a.payload }
+	},
+	{
+		case: ADD_MESSAGE_REQUESTED,
+		request: {
+			url: url,
+			onSuccess: res => ({ type: ADD_MESSAGE_SUCCEEDED }),
+			onFail: res => ({ type: ADD_MESSAGE_FAILED })
+		}
+	},
+	{
+		case: SEARCH_INPUT_CHARACTER_ENTERED,
+		debounce: 500,
+		post: {
+			url: autocomplete/a.payload,
+			cancelWhen: [
+				SEARCH_INPUT_CHARACTER_ENTERED,
+				SEARCH_INPUT_BLURED
+			],
+			onSuccess: res => ({ type: AUTOCOMPLETE_SUGGESTION })
+		}
+	},
+	{
+		case: [
+			CHAT_INPUT_SUBMITTED,
+			MESSANGER_INPUT_SUBMITED
+		],
+		debounce: 1000,
+		request: {
+			url: 'analyticsDb/a.type',
+			onSuccess: null
+			onFail:  null
+		}
+	}
+]
+*/
+
+function getActions(config, options, dispatcher) {
+  const actions = []
   const reducer = (state = {testState: 'testState'}, action) => {
-    lastAction = action
+    actions.push(action)
     return state
   }
   const store = createStore(reducer, applyMiddleware(orchestration(config, options)))
-  store.dispatch(testAction)
-  return lastAction
+  
+  dispatcher(store.dispatch)
+
+  return actions
 }
+
+function getLastAction(config, options, testAction) {
+  const actions = getActions(config, options, function (dispatch) {
+    dispatch(testAction)
+  })
+  return actions[actions.length - 1]
+}
+
 
 it('should dispatch action', () => {
   const testAction = { type: 'TEST' }
@@ -30,7 +118,7 @@ it('should not dispatch action', () => {
     .not.toEqual(testAction)
 })
 
-it('should transform action', () => {
+it('should transform actions', () => {
   const testAction = { type: 'TEST' }
   const options = { validate: true }
   const config = [
@@ -44,7 +132,7 @@ it('should transform action', () => {
     .toEqual({ type: 'AFTER_ORCHESTRATION' })
 })
 
-it('should transform action (case array format)', () => {
+it('should transform actions - case array format', () => {
   const options = { validate: true }
   const config = [
     {
@@ -60,7 +148,7 @@ it('should transform action (case array format)', () => {
     .toEqual({ type: 'AFTER_ORCHESTRATION' })
 })
 
-it('should transform action (case array format, dispatch object)', () => {
+it('should transform actions - dispatch object', () => {
   const options = { validate: true }
   const config = [
     {
@@ -76,7 +164,7 @@ it('should transform action (case array format, dispatch object)', () => {
     .toEqual({ type: 'AFTER_ORCHESTRATION' })
 })
 
-it('should transform action (dispatch function)', () => {
+it('should transform actions - dispatch function', () => {
   const testAction = { type: 'TEST' }
   const options = { validate: true }
   const config = [
@@ -90,7 +178,7 @@ it('should transform action (dispatch function)', () => {
     .toEqual({ type: 'AFTER_ORCHESTRATION_TEST_testState' })
 })
 
-it('multiple rules', () => {
+it('should transform actions - multiple rules', () => {
   const options = { validate: true }
   const config = [
     {
@@ -108,4 +196,112 @@ it('multiple rules', () => {
   
   expect(getLastAction(config, options, { type: 'TEST2' }))
     .toEqual({ type: 'AFTER_ORCHESTRATION2' })
+})
+
+it('should transform actions - delay', (done) => {
+  const options = { validate: true }
+  const config = [
+    {
+      case: 'TEST',
+      dispatch: 'DEBOUNCED',
+      delay: 50
+    }
+  ]
+
+  const actions = []
+  const reducer = (state, action) => {
+    actions.push(action)
+    return state
+  }
+  const store = createStore(reducer, applyMiddleware(orchestration(config, options)))
+  store.dispatch({ type: 'TEST' })
+    
+  if (actions.length !== 1) {
+    done.fail(`expected 1 dispatched action, got ${actions.length}`)
+  }
+
+  setTimeout(() => {
+    if (actions.length === 2) {
+      done()
+    } else {
+      done.fail(`expected 2 dispatched actions, got ${actions.length}`)
+    }
+  }, 100)
+})
+
+it('should transform actions - debounce', (done) => {
+  const options = { validate: true }
+  const config = [
+    {
+      case: 'TEST',
+      dispatch: 'DEBOUNCED',
+      debounce: 50
+    },
+    {
+      case: 'TEST2',
+      dispatch: 'DEBOUNCED',
+      debounce: 50
+    }
+  ]
+
+  const actions = []
+  const reducer = (state, action) => {
+    actions.push(action)
+    return state
+  }
+  const store = createStore(reducer, applyMiddleware(orchestration(config, options)))
+  
+  store.dispatch({ type: 'TEST' })
+  store.dispatch({ type: 'TEST' })
+  store.dispatch({ type: 'TEST' })
+  store.dispatch({ type: 'TEST2' })
+  store.dispatch({ type: 'TEST2' })
+  store.dispatch({ type: 'TEST2' })
+
+  setTimeout(() => {
+    const expectedNum = 3
+    if (actions.length === expectedNum) {
+      done()
+    } else {
+      done.fail(`expected ${expectedNum} dispatched actions, got ${actions.length}`)
+    }
+  }, 200)
+})
+
+it('should transform actions - delay debounce', (done) => {
+  const options = { validate: true }
+  const config = [
+    {
+      case: 'TEST',
+      dispatch: 'DEBOUNCED',
+      delay: 50,
+      debounce: 50
+    }
+  ]
+
+  const actions = []
+  const reducer = (state, action) => {
+    actions.push(action)
+    return state
+  }
+  const store = createStore(reducer, applyMiddleware(orchestration(config, options)))
+  
+  store.dispatch({ type: 'TEST' })
+  store.dispatch({ type: 'TEST' })
+  store.dispatch({ type: 'TEST' })
+  store.dispatch({ type: 'TEST' })
+  store.dispatch({ type: 'TEST' })
+
+  if (actions.length !== 1) {
+    done.fail(`expected 1 dispatched action, got ${actions.length}`)
+  }
+
+  setTimeout(() => {
+    const expectedNum = 2
+    if (actions.length === expectedNum) {
+      done()
+    } else {
+      done.fail(`expected ${expectedNum} dispatched actions, got ${actions.length}`)
+    }
+  }, 110)
 })
